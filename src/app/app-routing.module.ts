05482eca56f240815d81ea07f8edd48ef0d92d70
davidrs7 <davidrodriguezrivera0@gmail.com>
2021-03-22 17:32:01 -0500

    Tercer commit,salir con esté a producción!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InventariosComponent } from './pages/inventarios/inventarios.component';
 import { GridsComponent } from './pages/grids/grids.component';
-
+import { NavbarComponent } from './pages/resources/navbar/navbar.component';
+import { FooterComponent } from './pages/resources/footer/footer.component';
+  
 const routes: Routes = [
     {path: 'inventarios',component: InventariosComponent},
     {path: 'grids',component: GridsComponent},
+    {path: 'navbar',component: NavbarComponent},
+    {path: 'footer',component: FooterComponent},
     {path: '**', pathMatch: 'full', redirectTo: 'inventarios'}
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { InventariosComponent } from './pages/inventarios/inventarios.component'
 import { GridsComponent } from './pages/grids/grids.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './pages/resources/navbar/navbar.component';
+import { FooterComponent } from './pages/resources/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InventariosComponent,
-    GridsComponent
+    GridsComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
