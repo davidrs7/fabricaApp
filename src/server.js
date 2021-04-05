75c2c8416a0ca79359d5401c47fbc4e96c5d476a
davidrs7 <davidrodriguezrivera0@gmail.com>
2021-04-05 01:24:03 -0500

    deploy heroku

--- src/server.js
+++ src/server.js
@@ -1,14 +1,8 @@
+const express = require('express');
 const path = require('path');
-
 const app = express();
-
-app.use(express.static(__dirname+'/dist/blinders'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/blinders/index.html'));
-});
-
+app.use(express.static(__dirname + '/dist/blinders'));
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+
+    '/dist/blinders/index.html'));});
 app.listen(process.env.PORT || 8080);
-
-app.listen(process.env.PORT, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  })
