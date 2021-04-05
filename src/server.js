fe38a478b92ecc71df8ff543c5985f62c8b429fc
davidrs7 <davidrodriguezrivera0@gmail.com>
2021-04-05 00:53:04 -0500

    deploy heroku

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/blinders/index.html'));
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
 
 app.listen(process.env.PORT, () => {
     console.log(`Example app listening at http://localhost:${port}`)
